90da038013033a76707471c929e8f31aae35939b
Vadim Kamyshnikov <artskif83@gmail.com>
2026-02-20 15:08:24 +0400

    Обновлен метод toString() в классе TradeEvent: добавлены новые поля для отображения, включая stopLossPercentage, takeProfitPercentage и timeframe.

--- microservice-trading-analyzer/src/main/java/artskif/trader/events/trade/TradeEvent.java
+++ microservice-trading-analyzer/src/main/java/artskif/trader/events/trade/TradeEvent.java
@@ -1,9 +1,6 @@
 package artskif.trader.events.trade;
 
-import artskif.trader.strategy.event.common.Direction;
-import artskif.trader.strategy.event.common.Confidence;
 import artskif.trader.strategy.event.common.TradeEventData;
-import artskif.trader.strategy.event.common.TradeEventType;
 
 import java.time.Instant;
 
@@ -15,7 +12,17 @@ public record TradeEvent(
 ) {
     @Override
     public String toString() {
-        return "TradeEvent{" + tradeEventData.type() + ", " + instrument + ", " + tradeEventData.direction() + ", " + timestamp + ", isTest=" + isTest + "}";
+        return "TradeEvent{" +
+                "type=" + tradeEventData.type() +
+                ", instrument='" + instrument + '\'' +
+                ", direction=" + tradeEventData.direction() +
+                ", stopLossPercentage=" + tradeEventData.stopLossPercentage() +
+                ", takeProfitPercentage=" + tradeEventData.takeProfitPercentage() +
+                ", timeframe=" + tradeEventData.timeframe() +
+                ", eventPrice=" + tradeEventData.eventPrice() +
+                ", timestamp=" + timestamp +
+                ", isTest=" + isTest +
+                '}';
     }
 }
 
