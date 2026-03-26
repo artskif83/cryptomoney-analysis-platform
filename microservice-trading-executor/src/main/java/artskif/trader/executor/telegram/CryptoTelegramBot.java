e275ec6587771377fd19f7abc844b3d291141c28
Vadim Kamyshnikov <artskif83@gmail.com>
2026-03-26 15:19:02 +0400

    Обновлены методы сохранения буферов в классе AbstractCandle для передачи имени вызывающего метода. Изменен уровень логирования для класса CandleRepository и других компонентов на DEBUG. Обновлены параметры тейк-профита в классах GoldenFieldLongEventProcessor и GoldenFieldShortEventProcessor.

--- microservice-trading-executor/src/main/java/artskif/trader/executor/telegram/CryptoTelegramBot.java
+++ microservice-trading-executor/src/main/java/artskif/trader/executor/telegram/CryptoTelegramBot.java
@@ -16,69 +16,69 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
-@Component
-public class CryptoTelegramBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
-
-    private static final Logger log = LoggerFactory.getLogger(CryptoTelegramBot.class);
-
-    private final String botToken;
-    private final TelegramClient telegramClient;
-    @Getter
-    private Long lastChatId;
-
-    public CryptoTelegramBot(@Value("${BOT_TOKEN}") String botToken) {
-        this.botToken = botToken;
-        this.telegramClient = new OkHttpTelegramClient(botToken);
-        log.info("🤖 Telegram бот инициализирован");
-    }
-
-    @Override
-    public String getBotToken() {
-        return botToken;
-    }
-
-    @Override
-    public LongPollingUpdateConsumer getUpdatesConsumer() {
-        return this;
-    }
-
-    @Override
-    public void consume(Update update) {
-        // We check if the update has a message and the message has text
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            // Set variables
-            String message_text = update.getMessage().getText();
-            long chat_id = update.getMessage().getChatId();
-            lastChatId = chat_id; // Сохраняем chat_id
-
-            SendMessage message = SendMessage // Create a message object
-                    .builder()
-                    .chatId(chat_id)
-                    .text(message_text + " + ответ")
-                    .build();
-            try {
-                telegramClient.execute(message); // Sending our message object to user
-            } catch (TelegramApiException e) {
-                log.error("❌ Не удалось отправить сообщение в Telegram: {}", e.getMessage(), e);
-            }
-        }
-    }
-
-    public void sendToChat(String text) {
-        SendMessage message = SendMessage
-                .builder()
-                .chatId(lastChatId)
-                .text(text)
-                .build();
-        try {
-            telegramClient.execute(message);
-        } catch (TelegramApiException e) {
-            log.error("❌ Не удалось отправить сообщение в чат Telegram: {}", e.getMessage(), e);
-        }
-    }
-
-    @AfterBotRegistration
-    public void afterRegistration(BotSession botSession) {
-        log.info("✅ Зарегистрирован бот, статус работы: {}", botSession.isRunning());
-    }
-}
+//@Component
+//public class CryptoTelegramBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
+//
+//    private static final Logger log = LoggerFactory.getLogger(CryptoTelegramBot.class);
+//
+//    private final String botToken;
+//    private final TelegramClient telegramClient;
+//    @Getter
+//    private Long lastChatId;
+//
+//    public CryptoTelegramBot(@Value("${BOT_TOKEN}") String botToken) {
+//        this.botToken = botToken;
+//        this.telegramClient = new OkHttpTelegramClient(botToken);
+//        log.info("🤖 Telegram бот инициализирован");
+//    }
+//
+//    @Override
+//    public String getBotToken() {
+//        return botToken;
+//    }
+//
+//    @Override
+//    public LongPollingUpdateConsumer getUpdatesConsumer() {
+//        return this;
+//    }
+//
+//    @Override
+//    public void consume(Update update) {
+//        // We check if the update has a message and the message has text
+//        if (update.hasMessage() && update.getMessage().hasText()) {
+//            // Set variables
+//            String message_text = update.getMessage().getText();
+//            long chat_id = update.getMessage().getChatId();
+//            lastChatId = chat_id; // Сохраняем chat_id
+//
+//            SendMessage message = SendMessage // Create a message object
+//                    .builder()
+//                    .chatId(chat_id)
+//                    .text(message_text + " + ответ")
+//                    .build();
+//            try {
+//                telegramClient.execute(message); // Sending our message object to user
+//            } catch (TelegramApiException e) {
+//                log.error("❌ Не удалось отправить сообщение в Telegram: {}", e.getMessage(), e);
+//            }
+//        }
+//    }
+//
+//    public void sendToChat(String text) {
+//        SendMessage message = SendMessage
+//                .builder()
+//                .chatId(lastChatId)
+//                .text(text)
+//                .build();
+//        try {
+//            telegramClient.execute(message);
+//        } catch (TelegramApiException e) {
+//            log.error("❌ Не удалось отправить сообщение в чат Telegram: {}", e.getMessage(), e);
+//        }
+//    }
+//
+//    @AfterBotRegistration
+//    public void afterRegistration(BotSession botSession) {
+//        log.info("✅ Зарегистрирован бот, статус работы: {}", botSession.isRunning());
+//    }
+//}
